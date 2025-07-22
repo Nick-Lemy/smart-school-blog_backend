0a21a533f6989c546d2fe71853602db7b0c67db5
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-22 16:22:24 +0200

    ci: add user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-use.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.userService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get()
+  findOne(@Param() id: string) {
+    return this.userService.findOne(Number(id));
+  }
+
+  @Patch()
+  update(@Param() id: string, @Body() dto: UpdateUserDto) {
+    return this.userService.update(Number(id), dto);
+  }
+}
