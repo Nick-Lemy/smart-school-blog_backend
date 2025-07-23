252f804c62be7498201408c0c291629bebd5a414
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-24 00:30:55 +0200

    ci: user stuffs, done

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,7 +3,7 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-use.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Post()
@@ -16,12 +16,12 @@ export class UserController {
     return this.userService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param() id: string) {
+  @Get('/:id')
+  findOne(@Param('id') id: string) {
     return this.userService.findOne(Number(id));
   }
 
-  @Patch(':id')
+  @Patch('/:id')
   update(@Param() id: string, @Body() dto: UpdateUserDto) {
     return this.userService.update(Number(id), dto);
   }
