6659172c75e3a78317b0c147cd6238a74ac79ebd
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-24 16:53:26 +0200

    fix: try to fix up something

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,5 +2,5 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 export class LoginDto extends PartialType(CreateUserDto) {
-  id: number;
+  id?: number;
 }
