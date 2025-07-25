c7cdc8193b4c66cbd67814a66203729f76ac1126
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-25 07:14:16 +0200

    feat: add Event

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { EventService } from './event.service';
+import { CreateEventDto } from './dto/create-event.dto';
 
 @Controller('event')
-export class EventController {}
+export class EventController {
+  constructor(private readonly eventService: EventService) {}
+  @Post()
+  create(
+    @Body() dto: CreateEventDto,
+    @Req() req: { user: { userId: number } },
+  ) {
+    return this.eventService.create(req.user.userId, dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.eventService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.eventService.findOne(+id);
+  }
+
+  @Get('/user/:hostId')
+  findByHostId(@Param('hostId') hostId: string) {
+    return this.eventService.findByHostId(+hostId);
+  }
+
+  @Get('/category')
+  findByCategory(@Query('category') category: string) {
+    return this.eventService.findByCategory(category);
+  }
+}
