65ffe7c220121b955ffb19a0bd89ead63ff1b810
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-25 06:31:15 +0200

    ci: init event module service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
+import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, PostModule, CommentModule],
+  imports: [PrismaModule, UserModule, AuthModule, PostModule, CommentModule, EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
